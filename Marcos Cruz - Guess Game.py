7abb689ea069f28da5e6c2c1901e0b5c8a2e8bd7
marcosc14 <33762664+marcosc14@users.noreply.github.com>
2017-12-07 10:47:41 -0800

    worked on guess game
    12.7.17

--- Marcos Cruz - Guess Game.py
+++ Marcos Cruz - Guess Game.py
@@ -3,19 +3,31 @@
 import random  # This should be on line 1
 print(random.randint(1, 50))
 
-
-
-guess = input()
-
-num = 15
-
-15 == "15"
+print("Hi Welcome")
+guess = input("try to guess a number between 1 and 50")
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+#guess = input()
+
+#num = 15
+
+#15 == "15"
+
+#str(num) == guess
 
 
 
--- Warmups.py
+++ Warmups.py
@@ -5,14 +5,37 @@ def reverse_order():
     print("%s, %s" % (last_name, first_name)
 
 
-def add_py(name):
-    print("%s.py" % name)        #Solution 1
-    print(name + ".py")           #Solution 2
+#def add_py(name):
+   # print("%s.py" % name)        #Solution 1
+   # print(name + ".py")           #Solution 2
 
 
-def add(num1, num2, num3):
-    print(num1 + num2 + num3)
+#def add(num1, num2, num3):
+    #print(num1 + num2 + num3)
 
-add(90, 900, 9000)
+#add(90, 900, 9000)
+
+
+def repeat(string):
+    print(string)
+    print(string)
+    print(string)
+
+    for x in range(3):
+        print(string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
